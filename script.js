31503a47067bed9b8db29f02a0ffcc7eb57b3313
2uzuk1 <g2uzuk1m@gmail.com>
2026-07-12 12:16:11 +0900

    Adiciona validações no display

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insertToDisplay(data) {
-    document.querySelector('#display').value += data
+    const display = document.querySelector('#display')
+    if (display.value === 'Error') return 
+    if (display.value === '' && data === '.') return
+
+    const op = ['+', '-', '*', '/']
+    const lastOp = display.value.slice(-1)
+
+    if (op.includes(data) && (op.includes(lastOp) || lastOp === '.')) return
+    if (data === '.' && (lastOp === '.' || op.includes(lastOp) || display.value.split(/[+\-*/]/).pop().includes('.'))) return
+    
+    display.value += data
 }
 
 function clean(){
@@ -8,11 +18,17 @@ function clean(){
 
 function backspace(){
     let display = document.querySelector('#display')
+
+    if (display.value === 'Error') return
     display.value = display.value.slice(0, -1)
 }
 
 function result() {
     let display = document.querySelector('#display')
+
+    if (display.value === 'Error') return
+    if (display.value === '') return
+
     try {
         display.value = eval(display.value)
     } catch {
